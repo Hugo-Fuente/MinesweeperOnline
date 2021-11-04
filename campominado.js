7214e3f90dd10de1cc81479bdd32618ad2a880a7
Renan Ferretti <40603756+Renanferretti@users.noreply.github.com>
2021-11-04 16:41:37 -0300

    código atualizado
    Modificações no index: irá levar a uma página de configurações do jogo
    Modificações no style.css: nada demais, somente alguns casos específicos
    Configs: formulário para o envio das configurações de jogo
    Jogo: aplicação das configurações ao tabuleiro, faltando apenas o Rivotril
    Campominado.js: inclusão do timer, configurações e botão trapaça atualizado
    
    O que falta está na linha 1 do index.html

--- campominado.js
+++ campominado.js
@@ -26,6 +26,7 @@ function cadastrar() {
     return false;
 }
 
+
 function login() {
     var forms = document.forms["formulario"];
     var dados = {
@@ -53,56 +54,76 @@ function login() {
     return false;
 }
 
+
 function alterar() {
-    var forms = document.forms["formulario"];
-    var dados = {
-        'nome': forms["nome"].value,
-        'telefone': forms["telefone"].value,
-        'email': forms["email"].value,
-        'senha': forms["senha"].value,
-        'nascimento': dadosCadastrais['nascimento'],
-        'CPF': dadosCadastrais['CPF'],
-        'username': dadosCadastrais['username']
-    }
-    dadosCadastrais.push(dados);
-    alert("Seu perfil foi alterado");
-    forms.reset();
-    return false;
+  var forms = document.forms["formulario"];
+  var dados = {
+      'nome': forms["nome"].value,
+      'telefone': forms["telefone"].value,
+      'email': forms["email"].value,
+      'senha': forms["senha"].value
+  }
+
+  sessionStorage.setItem("nome", dados.nome);
+  sessionStorage.setItem("telefone", dados.telefone);
+  sessionStorage.setItem("email", dados.email);
+  sessionStorage.setItem("senha", dados.senha);
+
+  alert("Alteração realizada com sucesso");
+  forms.reset(); // resetando o forms
+  return false;
 }
 
 
 /*---------------------- JOGO ------------------------*/
 
-const comecarMinutos = 10;
-let tempo = comecarMinutos * 60;
-
-const temporizadorEl = document.getElementById('tempo');
-
-setInterval(updateCountdown, 1000);
-
-function updateCountdown() {
-    let minutos = Math.floor(tempo / 60);
-    minutos = minutos < 10 ? '0' + minutos : minutos; //se minutos < 10, adicionar 0 a esquerda. Ex: 07:23
-
-    let segundos = tempo % 60;
-    segundos = segundos < 10 ? '0' + segundos : segundos; //se segundos < 10, adicionar 0 a esquerda. Ex: 02:09
-    
-    temporizadorEl.innerHTML = `${minutos}:${segundos}`;
-    
-    tempo--;
-    tempo = tempo < 0 ? 0 : tempo; //impedir que o temporizador passe para numeros negativos
-}
-
 var grid = document.getElementById("grid");
-var testMode = false; //Turn this variable to true to see where the mines are
+var testMode = false; // Vira True quando clicamos no botão trapaça
+dimensao = parseInt(sessionStorage.getItem("dimensaoX"));
 generateGrid();
 
+
+function configs() {
+  var modo = document.getElementById("modalidade");
+  var forms = document.forms["formulario"];
+  var dados = {
+    'dimensaoX': forms["dimensaoX"].value,
+    'dimensaoY': forms["dimensaoY"].value,
+    'bombas': forms["bombas"].value,
+    'modalidade': modo.value
+  }
+
+  if (checagem(dados.dimensaoX, dados.dimensaoY, dados.bombas)) {
+    sessionStorage.setItem("dimensaoX", dados.dimensaoX);
+    sessionStorage.setItem("dimensaoY", dados.dimensaoY);
+    sessionStorage.setItem("bombas", dados.bombas);
+    sessionStorage.setItem("modalidade", dados.modalidade);
+  
+    forms.reset();
+    window.location.href = "jogo.html";
+    return false;
+  } else {
+    document.getElementById("invalido").innerHTML = 
+    "O número de bombas deve respeitar o tamanho do grid";
+    return false;
+  }
+}
+
+
+function checagem(x, y, bombas) {
+  if (bombas > x*y) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
 function generateGrid() {
-  //generate 10 by 10 grid
-  grid.innerHTML="";
-  for (var i=0; i<10; i++) {
+  grid.innerHTML = "";
+  for (var i = 0; i < dimensao; i++) {
     row = grid.insertRow(i);
-    for (var j=0; j<10; j++) {
+    for (var j = 0; j < dimensao; j++) {
       cell = row.insertCell(j);
       cell.onclick = function() { clickCell(this); };
       var mine = document.createAttribute("data-mine");       
@@ -110,67 +131,142 @@ function generateGrid() {
       cell.setAttributeNode(mine);
     }
   }
+  infos();
   addMines();
 }
 
-function addMines() {
-  //Add mines randomly
-  for (var i=0; i<20; i++) {
-    var row = Math.floor(Math.random() * 10);
-    var col = Math.floor(Math.random() * 10);
-    var cell = grid.rows[row].cells[col];
-    cell.setAttribute("data-mine","true");
-    if (testMode) cell.innerHTML="X";
+
+function infos() {
+  var txtDimensaoX = document.getElementById("dimensao");
+  var txtDimensaoY = document.getElementById("dimensao");
+  txtDimensaoX = sessionStorage.getItem("dimensaoX");
+  txtDimensaoY = sessionStorage.getItem("dimensaoY");
+  document.getElementById("dimensao").innerHTML = txtDimensaoX + "x" + txtDimensaoY;
+
+  var txtBombas = document.getElementById("bombas");
+  txtBombas = sessionStorage.getItem("bombas");
+  document.getElementById("bombas").innerHTML = txtBombas;
+
+  document.getElementById("modalidade").innerHTML = sessionStorage.getItem("modalidade");
+
+  //var txtRivotril = document.getElementById("rivotril");
+  //document.getElementById("rivotril").innerHTML = 2;
+}
+
+
+// criando o cronometro do jogo
+let segundos = 0;
+let minutos = 0;
+setInterval(function(){ tempo() }, 1000);
+
+function tempo() {        
+  const cronometro = document.getElementById('tempo');
+  segundos++;
+  segundos = segundos < 10 ? '0' + segundos : segundos;
+
+  if(segundos == 60) {
+    minutos++;
+    segundos = 0;
+    minutos = minutos < 10 ? '0' + minutos : minutos;
+  }
+  cronometro.innerHTML = `${minutos}:${segundos}`;
+}
+
+
+function inciarRivotril(){
+  if (sessioStorage.getItem("modalidade") == 'rivotril') {
+    const comecarMinutos = 10;
+    let temporestante = comecarMinutos * 60;
+    const temporizadorEl = document.getElementById('rivotril');
+    setInterval(timerRivotril, 1000); 
   }
 }
 
-function revealMines() {
-    //Highlight all mines in red
-    for (var i=0; i<10; i++) {
-      for(var j=0; j<10; j++) {
-        var cell = grid.rows[i].cells[j];
-        if (cell.getAttribute("data-mine")=="true") cell.className="mine";
-      }
+
+function timerRivotril() {
+  let minutos = Math.floor(temporestante / 60);
+  minutos = minutos < 10 ? '0' + minutos : minutos; //se minutos < 10, adicionar 0 a esquerda. Ex: 07:23
+
+  let segundos = temporestante % 60;
+  segundos = segundos < 10 ? '0' + segundos : segundos; //se segundos < 10, adicionar 0 a esquerda. Ex: 02:09
+
+  temporizadorEl.innerHTML = `${minutos}:${segundos}`;
+
+  temporestante--;
+  temporestante = temporestante < 0 ? 0 : temporestante; //impedir que o temporizador passe para numeros negativos
+}
+
+
+function addMines() {
+  for(var i = 0; i < parseInt(sessionStorage.getItem("bombas")); i++) {
+    var row = Math.floor(Math.random() * dimensao);
+    var col = Math.floor(Math.random() * dimensao);
+    var cell = grid.rows[row].cells[col];
+    cell.setAttribute("data-mine","true");
+    if (testMode) {
+      cell.innerHTML = "X";
     }
+  }
+}
+
+
+function revealMines() {
+  //Highlight all mines in red
+  for (var i = 0; i < dimensao; i++) {
+    for (var j = 0; j < dimensao; j++) {
+      var cell = grid.rows[i].cells[j];
+      if (cell.getAttribute("data-mine") == "true") cell.className = "mine";
+    }
+  }
 }
 
 function checkLevelCompletion() {
   var levelComplete = true;
-    for (var i=0; i<10; i++) {
-      for(var j=0; j<10; j++) {
-        if ((grid.rows[i].cells[j].getAttribute("data-mine")=="false") && (grid.rows[i].cells[j].innerHTML=="")) levelComplete=false;
+    for(var i = 0; i < dimensao; i++) {
+      for(var j = 0; j < dimensao; j++) {
+        if((grid.rows[i].cells[j].getAttribute("data-mine") == "false") 
+        && (grid.rows[i].cells[j].innerHTML == "")) {
+          levelComplete = false;
+        }
       }
   }
+
   if (levelComplete) {
-    alert("You Win!");
+    alert("VITÓRIA");
     revealMines();
   }
 }
 
+
 function clickCell(cell) {
   //Check if the end-user clicked on a mine
-  if (cell.getAttribute("data-mine")=="true") {
+  if (cell.getAttribute("data-mine") == "true") {
     revealMines();
     alert("Game Over");
   } else {
-    cell.className="clicked";
+    cell.className = "clicked";
     //Count and display the number of adjacent mines
-    var mineCount=0;
+    var mineCount = 0;
     var cellRow = cell.parentNode.rowIndex;
     var cellCol = cell.cellIndex;
     //alert(cellRow + " " + cellCol);
-    for (var i=Math.max(cellRow-1,0); i<=Math.min(cellRow+1,9); i++) {
-      for(var j=Math.max(cellCol-1,0); j<=Math.min(cellCol+1,9); j++) {
-        if (grid.rows[i].cells[j].getAttribute("data-mine")=="true") mineCount++;
+    for (var i = Math.max(cellRow-1,0); i <= Math.min(cellRow+1, dimensao-1); i++) {
+      for(var j = Math.max(cellCol-1,0); j <= Math.min(cellCol+1, dimensao-1); j++) {
+        if (grid.rows[i].cells[j].getAttribute("data-mine") == "true"){
+          mineCount++;
+        } 
       }
     }
-    cell.innerHTML=mineCount;
-    if (mineCount==0) { 
+
+    cell.innerHTML = mineCount;
+    if (mineCount == 0) { 
       //Reveal all adjacent cells as they do not have a mine
-      for (var i=Math.max(cellRow-1,0); i<=Math.min(cellRow+1,9); i++) {
-        for(var j=Math.max(cellCol-1,0); j<=Math.min(cellCol+1,9); j++) {
+      for (var i = Math.max(cellRow-1,0); i <= Math.min(cellRow+1, dimensao-1); i++) {
+        for(var j = Math.max(cellCol-1,0); j <= Math.min(cellCol+1, dimensao-1); j++) {
           //Recursive Call
-          if (grid.rows[i].cells[j].innerHTML=="") clickCell(grid.rows[i].cells[j]);
+          if (grid.rows[i].cells[j].innerHTML==""){
+            clickCell(grid.rows[i].cells[j]);
+          }
         }
       }
     }
@@ -178,17 +274,12 @@ function clickCell(cell) {
   }
 }
 
-function cheatButton(){
-    //Highlight all mines in red
-  for (var i=0; i<10; i++) {
-    for(var j=0; j<10; j++) {
-      var cell = grid.rows[i].cells[j];
-      if (cell.getAttribute("data-mine")=="true") cell.className="mine";
-    }
-  }
+
+function cheatButton() {
+  revealMines();
   setTimeout(function() { // Hides the highlighted mines after 5 seconds
-    for (var i=0; i<10; i++) {
-      for(var j=0; j<10; j++) {
+    for (var i = 0; i < dimensao; i++) {
+      for(var j = 0; j < dimensao; j++) {
         var cell = grid.rows[i].cells[j];
         if (cell.getAttribute("data-mine")=="true") cell.className="hidden";
       }
